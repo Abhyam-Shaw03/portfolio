41c988a20f4e0183b9c6f77bd97a48bec72d7041
Abhyam Shaw <abhyamshaw3@gmail.com>
2025-01-31 01:36:45 +0530

    Fix image paths to include repository base path

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 //using the top function instead of the down one beacuse of some changes made in vite.config.js
 
 export const getImageUrl = (path) => {
-  return `/assets/${path}`;
+  const basePath = "/portfolio";
+  return `${basePath}/assets/${path}`;
 };
 
 /*
